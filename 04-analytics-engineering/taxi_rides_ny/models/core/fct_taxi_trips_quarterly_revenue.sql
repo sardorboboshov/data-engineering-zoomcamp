6b4df8c59b029ecd522ee20604ec8028b659761a
SARDOR BOBOSHOV <sardortechlead@gmail.com>
2025-02-28 20:37:13 +0000

    adding fct_taxi_trips_quarterly_revenue

---
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
@@ -0,0 +1,18 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+trips_unioned as (
+    select * from fact_trips
+), 
+dim_datetime as (
+    select * from {{ ref('dim_datetime') }}
+    where borough != 'Unknown'
+)
+SELECT trips_unioned.service_type, dim_datetime.year, dim_datetime.quarter, 
+    SUM(total_amount)
+
+FROM trips_unioned inner join dim_datetime on trips_unioned.pickup_datetime = dim_datetime.pickup_datetime
+group by 1,2,3
