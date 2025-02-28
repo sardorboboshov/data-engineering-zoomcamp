5fd54ebd85ec9856d90147d25d465774cd47991d
SARDOR BOBOSHOV <sardortechlead@gmail.com>
2025-02-28 21:34:47 +0000

    a

--- 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_quarterly_revenue.sql
@@ -14,4 +14,5 @@ SELECT trips_unioned.service_type, dim_datetime.revenue_year, dim_datetime.reven
     SUM(total_amount) as total_amount
 
 FROM trips_unioned inner join dim_datetime on trips_unioned.pickup_datetime = dim_datetime.pickup_datetime
+where dim_datetime.revenue_year in (2019, 2020)
 group by 1,2,3
