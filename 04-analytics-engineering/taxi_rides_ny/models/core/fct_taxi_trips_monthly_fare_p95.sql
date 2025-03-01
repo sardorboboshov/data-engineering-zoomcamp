a80a23e0c8652bb05c9891d8b4ae31cb45d1efc5
SARDOR BOBOSHOV <sardortechlead@gmail.com>
2025-03-01 11:41:52 +0000

    1

--- 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
@@ -11,10 +11,12 @@ dim_datetime as (
 )
 SELECT 
     fact_trips.service_type, dim_datetime.year, dim_datetime.month,
-    PERCENTILE_CONT(0.9) WITHIN GROUP (ORDER BY fare_amount) AS p90_sale_amount,
-    PERCENTILE_CONT(0.95) WITHIN GROUP (ORDER BY fare_amount) AS p95_sale_amount,
-    PERCENTILE_CONT(0.97) WITHIN GROUP (ORDER BY fare_amount) AS p97_sale_amount
+    PERCENTILE_CONT(0.9) WITHIN GROUP (ORDER BY fact_trips.fare_amount) OVER(
+        PARTITION BY fact_trips.service_type,  dim_datetime.year, dim_datetime.month) AS p90_sale_amount,
+    PERCENTILE_CONT(0.95) WITHIN GROUP (ORDER BY fact_trips.fare_amount) OVER(
+        PARTITION BY fact_trips.service_type,  dim_datetime.year, dim_datetime.month) AS p95_sale_amount,
+    PERCENTILE_CONT(0.97) WITHIN GROUP (ORDER BY fact_trips.fare_amount) OVER(
+        PARTITION BY fact_trips.service_type,  dim_datetime.year, dim_datetime.month) AS p97_sale_amount,
 FROM fact_trips inner join dim_datetime on fact_trips.pickup_datetime = dim_datetime.pickup_datetime
 where fact_trips.fare_amount > 0 and fact_trips.trip_distance > 0 
     and fact_trips.payment_type_description not in  {{ var('payment_type_descriptions') }}
-group by 1,2,3
