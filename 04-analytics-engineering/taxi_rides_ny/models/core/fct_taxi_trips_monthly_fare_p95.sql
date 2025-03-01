d0cda0501cccf67d17d5a0a94fb16ddfe972dca2
SARDOR BOBOSHOV <sardortechlead@gmail.com>
2025-03-01 12:35:25 +0000

    1

--- 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
+++ 04-analytics-engineering/taxi_rides_ny/models/core/fct_taxi_trips_monthly_fare_p95.sql
@@ -10,11 +10,14 @@ dim_datetime as (
     select * from {{ ref('dim_datetime') }} 
 )
 SELECT 
-    fact_trips.service_type, dim_datetime.revenue_year, dim_datetime.revenue_month,
-    PERCENTILE_CONT(fact_trips.fare_amount, 0.9) OVER (PARTITION BY fact_trips.service_type, dim_datetime.revenue_year, dim_datetime.revenue_month) AS p90_sale_amount,
-    PERCENTILE_CONT(fact_trips.fare_amount, 0.95) OVER (PARTITION BY fact_trips.service_type, dim_datetime.revenue_year, dim_datetime.revenue_month) AS p95_sale_amount,
-    PERCENTILE_CONT(fact_trips.fare_amount, 0.97) OVER (PARTITION BY fact_trips.service_type, dim_datetime.revenue_year, dim_datetime.revenue_month) AS p97_sale_amount
-    
-FROM fact_trips inner join dim_datetime on fact_trips.pickup_datetime = dim_datetime.pickup_datetime
-where fact_trips.fare_amount > 0 and fact_trips.trip_distance > 0 
-    and fact_trips.payment_type_description in  ('Cash', 'Credit card')
+    DISTINCT fact_trips.service_type, dim_datetime.revenue_year, dim_datetime.revenue_month, p97_sale_amount, p95_sale_amount, p90_sale_amount
+    FROM (
+        SELECT 
+            fact_trips.service_type, dim_datetime.revenue_year, dim_datetime.revenue_month,
+            PERCENTILE_CONT(fact_trips.fare_amount, 0.9) OVER (PARTITION BY fact_trips.service_type, dim_datetime.revenue_year, dim_datetime.revenue_month) AS p90_sale_amount,
+            PERCENTILE_CONT(fact_trips.fare_amount, 0.95) OVER (PARTITION BY fact_trips.service_type, dim_datetime.revenue_year, dim_datetime.revenue_month) AS p95_sale_amount,
+            PERCENTILE_CONT(fact_trips.fare_amount, 0.97) OVER (PARTITION BY fact_trips.service_type, dim_datetime.revenue_year, dim_datetime.revenue_month) AS p97_sale_amount
+        FROM fact_trips inner join dim_datetime on fact_trips.pickup_datetime = dim_datetime.pickup_datetime
+        where fact_trips.fare_amount > 0 and fact_trips.trip_distance > 0 
+            and lower(fact_trips.payment_type_description) in ('cash', 'credit card')
+    )
